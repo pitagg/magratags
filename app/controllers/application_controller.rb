a1f3786ffbfcee5cbb6e619b93aab2b0e71ec025
Pitágoras G. Galiotto <pitagg@gmail.com>
2019-03-13 15:26:39 -0300

    Adicionando locale pt-BR como padrão

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+
+  protected
+
+
+    def set_locale
+      # TODO: Locale e time zone devem vir das configurações do usuário.
+      I18n.locale = 'pt-BR'
+      Time.zone = 'Brasilia'
+    end
 end
