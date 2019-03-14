c89282596e32d5679202aa073bd8ebc42d45c06b
Pitágoras G. Galiotto <pitagg@gmail.com>
2019-03-14 18:46:38 -0300

    Correção na busca de posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,8 @@ class PostsController < ApplicationController
   layout 'timeline'
 
   def index
-    @posts = Post.order(:published_at).by_author(params[:by_author]).by_search(params[:by_search])
+    @posts = Post.order(:published_at)
+    @posts = @posts.by_author(params[:by_author]) if params[:by_author].present?
+    @posts = @posts.by_search(params[:by_search]) if params[:by_search].present?
   end
 end
